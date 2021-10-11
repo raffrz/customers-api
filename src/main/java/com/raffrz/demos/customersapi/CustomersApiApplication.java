d662741a1d31d2d9c214463c4b990e5d78ec7ae6
raffrz <ralves96@outlook.com>
2021-10-11 10:59:49 -0300

    Inicializacao do projeto com Spring Initializr

---
+++ src/main/java/com/raffrz/demos/customersapi/CustomersApiApplication.java
@@ -0,0 +1,13 @@
+package com.raffrz.demos.customersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raffrz/demos/customersapi/CustomersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raffrz.demos.customersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
